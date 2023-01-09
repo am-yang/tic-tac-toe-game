228586891b0ecd792241fb455efd2a754e765b14
Anita Yang <anita@Anitas-MacBook-Pro-8.local>
2023-01-09 17:41:14 +1100

    Implemented win logic

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -8,6 +8,7 @@ public class TicTacToe {
     private final int DIMENSION;
     private JFrame gameFrame;
     private JButton[][] buttons;
+    private JLabel winMessage;
     private int buttonY = 20;
     private int buttonX = 20;
     private boolean xTurn = true; // X player is always first
@@ -15,6 +16,11 @@ public class TicTacToe {
     public TicTacToe(int dimension){
         DIMENSION = dimension;
         initialiseGameFrame();
+        winMessage = new JLabel();
+        winMessage.setBounds(170, 550, 200, 50);
+        winMessage.setFont(new Font("Courier New", Font.PLAIN, 30));
+        winMessage.setForeground(new Color(34,139,34));
+        gameFrame.getContentPane().add(winMessage);
         initialiseButtons();
     }
 
@@ -34,12 +40,18 @@ public class TicTacToe {
                             if (((JButton)e.getSource()).getText().equals("")){
                                 ((JButton)e.getSource()).setText("X");
                                 xTurn = false;
+                                if (isWinner("X")){
+                                    winMessage.setText("X WINS!!!");
+                                }
                             }
                         }
                         else {
                             if (((JButton)e.getSource()).getText().equals("")){
                                 ((JButton)e.getSource()).setText("O");
                                 xTurn = true;
+                                if (isWinner("O")){
+                                    winMessage.setText("O WINS!!!");
+                                }
                             }
                         }
                     }
@@ -50,6 +62,65 @@ public class TicTacToe {
             buttonX = 20;
             buttonY += size;
         }
+    }
+
+    private boolean isWinner(String currPlayer){
+        int rowCount = 0;
+        int columnCount = 0;
+        int rightDiagonalCount = 0;
+        int leftDiagonalCount = 0;
+        // First checking if there are horizontal wins
+        RowOuterLoop:
+        for (int row = 0; row < DIMENSION; row++){
+            for (int column = 0; column < DIMENSION; column++){
+                if (buttons[row][column].getText().equals(currPlayer)){
+                    rowCount++;
+                    if (rowCount == DIMENSION){
+                        break RowOuterLoop;
+                    }
+                }
+                else {
+                    rowCount = 0;
+                    break;
+                }
+            }
+        }
+        // Now checking for vertical wins
+        ColumnOuterLoop:
+        for (int column = 0; column < DIMENSION; column++) {
+            for (int row = 0; row < DIMENSION; row++) {
+                if (buttons[row][column].getText().equals(currPlayer)){
+                    columnCount++;
+                    if (columnCount == DIMENSION){
+                        break ColumnOuterLoop;
+                    }
+                }
+                else {
+                    columnCount = 0;
+                    break;
+                }
+            }
+        }
+        // Now checking for diagonal wins
+        for (int index = 0; index < DIMENSION; index++){
+            if (buttons[index][index].getText().equals(currPlayer)){
+                rightDiagonalCount++;
+            }
+            else {
+                break;
+            }
+        }
+        for (int index = 0; index < DIMENSION; index++){
+            if (buttons[index][DIMENSION - 1 - index].getText().equals(currPlayer)){
+                leftDiagonalCount++;
+            }
+            else {
+                break;
+            }
+        }
+        return rowCount == DIMENSION || columnCount == DIMENSION || rightDiagonalCount == DIMENSION ||
+                leftDiagonalCount == DIMENSION;
+
     }
 
     private void initialiseGameFrame(){
